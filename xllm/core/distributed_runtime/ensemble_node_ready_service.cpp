dacb6994ae62c0d31fe4eee992782ba7df87a34b
wangyunlong.115 <wangyunlong.115@jd.com>
2026-04-29 18:30:27 +0800

    feat: add ensemble node ready service.

---
+++ xllm/core/distributed_runtime/ensemble_node_ready_service.cpp
@@ -0,0 +1,86 @@
+/* Copyright 2026 The xLLM Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://github.com/jd-opensource/xllm/blob/main/LICENSE
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "distributed_runtime/ensemble_node_ready_service.h"
+
+#include <absl/time/clock.h>
+#include <absl/time/time.h>
+#include <brpc/closure_guard.h>
+#include <glog/logging.h>
+
+namespace xllm {
+
+EnsembleNodeReadyService::EnsembleNodeReadyService(int32_t total_num)
+    : total_num_(total_num) {
+  CHECK_GE(total_num_, 0) << "total_num must be non-negative.";
+}
+
+bool EnsembleNodeReadyService::register_ready(const std::string& node_name,
+                                              const std::string& target) {
+  if (node_name.empty()) {
+    LOG(ERROR) << "node_name cannot be empty.";
+    return false;
+  }
+  if (target.empty()) {
+    LOG(ERROR) << "target cannot be empty.";
+    return false;
+  }
+
+  std::lock_guard<std::mutex> lock(mutex_);
+  auto ready_it = ready_targets_.find(node_name);
+  if (ready_it != ready_targets_.end()) {
+    if (ready_it->second == target) {
+      return true;
+    }
+    LOG(ERROR) << "node already registered with different target: "
+               << node_name;
+    return false;
+  }
+
+  ready_targets_.emplace(node_name, target);
+  return true;
+}
+
+std::unordered_map<std::string, std::string> EnsembleNodeReadyService::wait(
+    int64_t timeout_ms) {
+  CHECK_GE(timeout_ms, 0) << "timeout_ms must be non-negative.";
+  const absl::Time deadline = absl::Now() + absl::Milliseconds(timeout_ms);
+  while (true) {
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      if (static_cast<int32_t>(ready_targets_.size()) >= total_num_) {
+        return ready_targets_;
+      }
+    }
+    if (absl::Now() >= deadline) {
+      LOG(ERROR) << "Timeout waiting ensemble node ready.";
+      std::lock_guard<std::mutex> lock(mutex_);
+      return ready_targets_;
+    }
+    absl::SleepFor(absl::Milliseconds(100));
+  }
+}
+
+void EnsembleNodeReadyService::RegisterReady(
+    ::google::protobuf::RpcController* controller,
+    const proto::EnsembleNodeReadyRequest* request,
+    proto::EnsembleNodeReadyResponse* response,
+    ::google::protobuf::Closure* done) {
+  brpc::ClosureGuard done_guard(done);
+
+  response->set_ok(register_ready(request->node_name(), request->target()));
+}
+
+}  // namespace xllm
---
+++ xllm/core/distributed_runtime/ensemble_node_ready_service.h
@@ -0,0 +1,50 @@
+/* Copyright 2026 The xLLM Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://github.com/jd-opensource/xllm/blob/main/LICENSE
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#pragma once
+
+#include <cstdint>
+#include <mutex>
+#include <string>
+#include <unordered_map>
+
+#include "common/macros.h"
+#include "worker.pb.h"
+
+namespace xllm {
+
+class EnsembleNodeReadyService final : public proto::EnsembleNodeReady {
+ public:
+  explicit EnsembleNodeReadyService(int32_t total_num);
+  ~EnsembleNodeReadyService() override = default;
+
+  std::unordered_map<std::string, std::string> wait(int64_t timeout_ms);
+
+  void RegisterReady(::google::protobuf::RpcController* controller,
+                     const proto::EnsembleNodeReadyRequest* request,
+                     proto::EnsembleNodeReadyResponse* response,
+                     ::google::protobuf::Closure* done) override;
+
+ private:
+  bool register_ready(const std::string& node_name, const std::string& target);
+
+  DISALLOW_COPY_AND_ASSIGN(EnsembleNodeReadyService);
+
+  int32_t total_num_ = 0;
+  mutable std::mutex mutex_;
+  std::unordered_map<std::string, std::string> ready_targets_;
+};
+
+}  // namespace xllm
